7af3a6e16d559a950519b67232e758a43f34a812
delani <delanicwele68@gmail.com>
2024-06-13 22:13:59 +0200

    user-component and task  component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { UserComponent } from '../user/user.component';
+import { TasksComponent } from '../tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: String;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
